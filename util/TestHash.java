1e77e0849e0f9a175973a916a09917017d899b06
Christopher Lyod Mercado <dms.prime3101@gmail.com>
2025-12-29 20:54:56 +0800

    Delete util directory

--- util/TestHash.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public class TestHash {
-    public static void main(String[] args) {
-        String password = "admin123";
-        System.out.println(SecurityUtil.hashPassword(password));
-    }
-}
